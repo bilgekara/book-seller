b39aa95528400b2e7d127c5f1591847916e2c3bf
bilgekara <karabilgenur@gmail.com>
2022-07-14 18:28:24 +0300

    fix: little bug fixing and Purchase Controller

--- src/main/java/com/bilgeli/bookseller/service/PurchaseHistoryService.java
+++ src/main/java/com/bilgeli/bookseller/service/PurchaseHistoryService.java
@@ -4,10 +4,12 @@ import com.bilgeli.bookseller.model.PurchaseHistory;
 import com.bilgeli.bookseller.repository.IPurchaseHistoryRepository;
 import com.bilgeli.bookseller.repository.projection.IPurchaseItem;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Service
 public class PurchaseHistoryService implements IPurchaseHistoryService{
 
     @Autowired
