a0c081c4af791c6fd2e41c39ee03d295247fb140
bilgekara <karabilgenur@gmail.com>
2022-07-14 13:37:12 +0300

    fix: jwt authorization

---
+++ src/main/java/com/bilgeli/bookseller/security/jwt/IJwtProvider.java
@@ -0,0 +1,14 @@
+package com.bilgeli.bookseller.security.jwt;
+
+import com.bilgeli.bookseller.security.UserPrincipal;
+import org.springframework.security.core.Authentication;
+
+import javax.servlet.http.HttpServletRequest;
+
+public interface IJwtProvider {
+    String generateToken(UserPrincipal auth);
+
+    Authentication getAuthentication(HttpServletRequest request);
+
+    boolean validateToken(HttpServletRequest request);
+}
